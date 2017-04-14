1d0e2435b82ad1b14013b4d68bdf2819b8be832e
Philippe M. Chiasson <gozer@ectoplasm.org>
2017-04-14 10:07:27 -0400

    s3 sync tyop

--- src/org/mozilla/nubis/Static.groovy
+++ src/org/mozilla/nubis/Static.groovy
@@ -8,7 +8,7 @@ def buildSite() {
   sh "rsync -a --delete --cvs-exclude src/ dst/"
 }
 
-def syncSite(args="--delete --acl public-read --only-show-errors") {
+def syncSite(args="--delete") {
   //sh "rclone -q sync ${excludes} dst/ s3:${env.SITE_BUCKET}/${env.JOB_BASE_NAME}/"
   //sh "rclone -q sync ${excludes} dst/ s3:${env.SITE_BUCKET}/"
   sh "aws s3 sync ${args} dst/ s3://${env.SITE_BUCKET}/"
