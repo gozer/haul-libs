2885bdff99516b3abdd19a3ac18c0ee1b6376d85
Philippe M. Chiasson <gozer@ectoplasm.org>
2017-04-19 10:23:01 -0400

    set acl when syncing

--- src/org/mozilla/nubis/Static.groovy
+++ src/org/mozilla/nubis/Static.groovy
@@ -8,6 +8,6 @@ def buildSite() {
   sh "rsync -a --delete --cvs-exclude src/ dst/"
 }
 
-def syncSite(args="--delete") {
+def syncSite(args="--acl public-read --delete") {
   sh "aws --region \"\$(nubis-region)\" s3 sync ${args} dst/ s3://${env.SITE_BUCKET}/"
 }
